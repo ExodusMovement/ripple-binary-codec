f5dbb4207d50c606edeb641aa941fe0320e3aaa7
Guten Ye <ywzhaifei@gmail.com>
2020-05-29 10:52:25 +0800

    make decimal.js work on node and webpack

--- src/quality.js
+++ src/quality.js
@@ -1,4 +1,4 @@
-const Decimal = require('decimal.js');
+const Decimal = require('decimal.js').default;
 const {bytesToHex, slice, parseBytes} = require('./utils/bytes-utils');
 const {UInt64} = require('./types');
 const BN = require('bn.js');
